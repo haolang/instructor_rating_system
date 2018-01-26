12ff0f15a102c988353567c49cfd738d4fd0157e
541124010 <541124010@qq.com>
2018-01-26 17:05:10 +0800

    php

--- php/oauth_log.php
+++ php/oauth_log.php
@@ -1,7 +1,17 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Shinelon
- * Date: 2018/1/23
- * Time: 14:35
+ * User: 呆呆熊
+ * Date: 2018/1/19
+ * Time: 22:17
  */
+/*
+ * 判断页面的来源
+ */
+$url=$_SERVER['HTTP_REFERER'];
+if(strpos($url,'index.html')){
+    header('Location: https://www.yiban.cn/login?go=http://app.yiban.cn');//登录页是这个吗
+}
+else if(strpos($url,'statistic.html')||strpos($url,'(statistic_detail.php')||strpos($url,'admin.php')){
+    header('Location: admin_log.php');
+}
