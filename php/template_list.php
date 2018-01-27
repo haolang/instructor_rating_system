2c7127cb0e3bf94d4f32f7545ba5a4ddf169a2a5
541124010 <541124010@qq.com>
2018-01-28 01:20:52 +0800

    template_list.php

--- php/template_list.php
+++ php/template_list.php
@@ -1,7 +1,54 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Shinelon
- * Date: 2018/1/23
- * Time: 14:36
+ * User: 呆呆熊
+ * Date: 2018/1/21
+ * Time: 22:11
  */
+class requestResponse {
+    public $Status = "";
+    public $StatusCode = "";
+    public $Description="";
+    public $Error = "";
+    public $Ret_Data=array(
+
+    );
+}
+include_once'json_admin.php';
+$retResult = new requestResponse();//一个返回对象
+$template=array(
+    "title"=>"",
+    "c_time"=>"",
+    "template_id"=>"",
+    "distribution1_num"=>''  //不清楚这个是什么
+);
+$sql='SELECT * FROM tbl_quetemplate  ';
+if($result=mysqli_query($dbcon,$sql))
+{
+    $retResult->Status= "success";
+    $retResult->StatusCode = 1;
+    $retResult->Description="";
+    $retResult->Error="";
+    while($row=$result->fetch_assoc())
+    {
+        $template['title']=$row['template_id'];
+        $template['c_time']=$row['c_time'];
+        $template['template_title']=$row['template_title'];
+        array_push($retResult->Ret_Data, $template);
+    }
+    mysqli_free_result($result);
+    $dbcon->close();
+    exit(json_encode($retResult));
+
+}
+else{
+    $retResult->Status= "failed";
+    $retResult->StatusCode = 0;
+    $retResult->Description="";
+    $retResult->Error="";
+    $retResult->Ret_Data="";
+    $dbcon->close();
+    exit(json_encode($retResult));//失败返回相关信息
+
+}
+
