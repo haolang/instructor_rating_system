392f86ce938f59826a9674e25d81dadc58a627f7
haolang <haolangtaiye@gmail.com>
2018-02-07 15:03:48 +0800

    测试前端代码暂时使用

---
+++ php/template_que_items.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shinelon
+ * Date: 2018/2/7
+ * Time: 15:01
+ */
+
+//todo 测试暂时使用
+echo '{
+    "Status":"success",
+    "StatusCode":1,
+    "Description":"",
+    "Error":"",
+    "Ret_Data":[
+        {
+            "q_id":1,
+            "content":"将数据存储于数据库2",
+            "score":10,
+            "selectors":[
+                {
+                    "s_id":1,
+                    "mark":"A",
+                    "content":"好",
+                    "percent":95
+                },
+                {
+                    "s_id":2,
+                    "mark":"B",
+                    "content":"一般",
+                    "percent":80
+                }
+            ]
+        },
+        {
+            "q_id":2,
+            "content":"将数据存储库2",
+            "score":10,
+            "selectors":[
+                {
+                    "s_id":1,
+                    "mark":"A",
+                    "content":"好",
+                    "percent":95
+                },
+                {
+                    "s_id":2,
+                    "mark":"B",
+                    "content":"一般",
+                    "percent":80
+                }
+            ]
+        }
+    ]
+}';
