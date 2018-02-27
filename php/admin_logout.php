ae5b886ad90ab0bfb6b973c330d21780b7ff4629
linkerGitHub <linker-junhao@outlook.com>
2018-02-28 00:06:58 +0800

    -注销功能

---
+++ php/admin_logout.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Linker
+ * Date: 2017/12/3
+ * Time: 15:51
+ */
+session_start();
+// 重置会话中的所有变量
+$_SESSION = array();
+
+// 如果要清理的更彻底，那么同时删除会话 cookie
+// 注意：这样不但销毁了会话中的数据，还同时销毁了会话本身
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// 最后，销毁会话
+session_destroy();
